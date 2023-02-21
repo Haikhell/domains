6fc9d7e90eb73d2c36d7f0244d3bcd6e4d3bd915
Ivan Haikhel <ivanhaikhell@gmail.com>
2023-02-22 00:13:37 +0200

    clear config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,5 @@
 module.exports = Object.freeze({
   NODE_ENV: process.env.NODE_ENV,
-  token: process.env.token,
   API_USER_ID: process.env.API_USER_ID,
   API_SECRET: process.env.API_SECRET,
   STEP_LOGS: process.env.STEP_LOGS
